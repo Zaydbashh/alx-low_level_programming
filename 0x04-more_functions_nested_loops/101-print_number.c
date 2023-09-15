db08e55b55ffa4b6ca31912de5602c6d095b9f16
Zaydbashh <bishirZ187@gmail.com>
2023-09-14 18:59:18 -0700

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - prints an integer
-  * @n: integer to be printed
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
@@ -21,4 +21,6 @@ n1 = n;
 if (n1 / 10)
 {
 print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,8 @@ printf(" Fizz");
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
 }
